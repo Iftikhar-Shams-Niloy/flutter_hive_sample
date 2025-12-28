a5d23a41d2ae6d936bcfbf5d4ecfb7bca7e50bd6
Iftikhar-Shams-Niloy <niloyshams21@gmail.com>
2025-12-28 23:47:38 +0600

    Initialize Hive and implement basic CRUD operations in HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hive_sample/screens/home_screen.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  //* HIVE is initilized here ⬇️
+  await Hive.initFlutter();
+
+  //* HIVE BOX opening 
+  var myBox = await Hive.openBox("myBox");
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      title: 'Hive Sample',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.orangeAccent)),
       home: const HomeScreen(),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _myBox = Hive.box("my_box");
+
+  //* <--- READ function --->
+  void readData(){
+    print(_myBox.get("key-1"));
+  }
+
+  //* <--- WRITE function --->
+  void writeData(){
+    _myBox.put("key-1", "This is test value for 'key-1'...");
+  }
+  
+
+  //* <--- DELETE function --->
+  void deleteData(){
+    _myBox.delete("key-1");
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Center(
+        child: Row(
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent.shade200,
+                foregroundColor: Colors.yellow.shade50,
+              ),
+              child: const Text("Read"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent.shade400,
+                foregroundColor: Colors.yellow.shade100,
+              ),
+              child: Text("Write"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent.shade700,
+                foregroundColor: Colors.yellow.shade200,
+              ),
+              child: Text("Delete"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
